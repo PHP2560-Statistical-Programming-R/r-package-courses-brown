61e497d02dcfc83ae3bbcc26dd74f8bf30fe54d4
pwqq <weiqi_pan@Brown.edu>
2017-11-29 11:44:02 -0500

    test

---
+++ test .R
@@ -0,0 +1,15 @@
+
+s <- html_session("https://bulletin.brown.edu/the-college/concentrations/")
+s <- s %>% follow_link("Economics"[2])
+
+webpage <- read_html(s)
+
+sb_table <- html_nodes(webpage, 'table')
+sb <- html_table(sb_table)[[1]]
+sb
+
+sb$X1
+sb$X2
+test <- data.frame(sb$X1, sb$X2)
+test
+
