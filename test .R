f48be20d9ca1c69ed6626df8f5cf2eb5a7177e97
Carolina Velasco <maria_carolina_velasco_molina@brown.edu>
2017-11-29 12:08:45 -0500

    conc.req

--- test .R
+++ test .R
@@ -1,15 +1,25 @@
+install.packages("rvest")
+library(rvest)
 
-s <- html_session("https://bulletin.brown.edu/the-college/concentrations/")
-s <- s %>% follow_link("Economics"[2])
+conc.req <- function(concentration_name) {
+  # This function takes the concentration of interest as an input and returns a 
+  # table with all the required classes for that particular concentration 
+  
+  # Pull up the website that has a list of all the undergraduate concentrations
+  link <- html_session("https://bulletin.brown.edu/the-college/concentrations/")
+  # Select the concentration of interest
+  link_conc <- link %>% follow_link("concentration_name")
+  # Read the content of the link
+  content <- read_html(link_conc)
+  # Scrape the table
+  link_table <- html_nodes(content, 'table')
+  scrape_table <- html_table(link_table)[[1]]
+  # Convert the table into a dataframe  
+  classes <- scrape_table$X1
+  description <- scrape_table$X2
+  conc_table <- data.frame(classes, description)
+  # Return the table
+  return(conc_table)
+}
 
-webpage <- read_html(s)
-
-sb_table <- html_nodes(webpage, 'table')
-sb <- html_table(sb_table)[[1]]
-sb
-
-sb$X1
-sb$X2
-test <- data.frame(sb$X1, sb$X2)
-test
-
+conc.req("Anthropology")
