484f4b00fa1927414728a3844ecf8f17d629a58a
Carolina Velasco <maria_carolina_velasco_molina@brown.edu>
2017-11-30 12:34:51 -0500

    list of concentrations

--- conc_req.R
+++ conc_req.R
@@ -1,12 +1,58 @@
-install.packages("rvest")
 library(rvest)
 library(dplyr)
+library(stringr)
+
+
+# If the table doesn't appear, then the concentration doesn't list the requirements, refer to...
+# Make input case insensitive, look for similar words
+
+
+# Compile a list of undergraduate concentrations available at Brown from the website, so 
+# that if the concentrations are updated on the website, the list is also updated
+link <- html_session("https://bulletin.brown.edu/the-college/concentrations/") 
+conc.list <- link %>% 
+  html_nodes("#textcontainer li") %>%
+  html_text()
+
+conc.list <- data_frame(conc.list)
+
+
+
+### conc.re function
+# This function takes the concentration of interest as an input and returns a 
+# table with all the required classes for that particular concentration 
+conc.req <- function(concentration_name) {
+  if (grepl(concentration_name, conc.list, ignore.case=TRUE)) {
+    # Pull up the website that has a list of all the undergraduate concentrations
+    link <- html_session("https://bulletin.brown.edu/the-college/concentrations/")
+    # Select the concentration of interest
+    link_conc <- link %>% follow_link(concentration_name)
+    # Read the content of the link
+    content <- read_html(link_conc)
+    # Scrape the table
+    link_table <- html_nodes(content, 'table')
+    scrape_table <- html_table(link_table)[[1]]
+    # Convert the table into a dataframe  
+    classes <- scrape_table$X1
+    description <- scrape_table$X2
+    number_classes <- scrape_table$X3
+    # Return the table
+    return(data_frame(classes, description, number_classes))
+    
+    }else stop('Please enter a valid concentration name. Refer to the list of undergraduate concentrations offered at Brown at https://bulletin.brown.edu/the-college/concentrations/')
+}
+  
+table_req <- conc.req("Music") 
+
+
+
+#if (str_detect(concentration_name, pattern= ) == TRUE) {
+
+
+
+
 
 conc.req <- function(concentration_name) {
-  
-  # This function takes the concentration of interest as an input and returns a 
-  # table with all the required classes for that particular concentration 
-  
   # Pull up the website that has a list of all the undergraduate concentrations
   link <- html_session("https://bulletin.brown.edu/the-college/concentrations/")
   # Select the concentration of interest
@@ -25,3 +71,4 @@ conc.req <- function(concentration_name) {
 }
 
 table_req <- conc.req("Music")
+ 
