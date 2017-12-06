a6e73e87a84dc9148847d918153dd276aa5af64f
Carolina Velasco <maria_carolina_velasco_molina@brown.edu>
2017-12-06 13:07:37 -0500

    package set up

---
+++ BDegreeAdvisor/R/conc_comp.R
@@ -0,0 +1,82 @@
+## Lis of available concentrations -------------------
+# Compile a list of undergraduate concentrations available at Brown from the website, so 
+# that if the concentrations are updated on the website, the list is also updated
+link <- html_session("https://bulletin.brown.edu/the-college/concentrations/") 
+conc_list <- link %>% 
+  html_nodes("#textcontainer li") %>% # css selector for the entire list of concentration
+  html_text() # select only the text 
+conc_list <- as.vector(conc_list)
+
+
+## Concentration 1 -------------------
+table_1 <- function(conc_name1) {
+  ## Run the function only if the user's input matches the name listed in the concentration 
+  ## list, ignoring cases. 
+  match <- grepl(pattern=paste("^", conc_name1,"$", sep=""), conc_list, ignore.case=TRUE)
+  # Index to find the link of the concentraton of interest
+  i <- grep(pattern=paste("^", conc_name1,"$", sep=""), conc_list, ignore.case=TRUE)
+  if (any(match==TRUE) && str_length(conc_name1) == str_length(conc_list[i])) {
+    # Pull up the website that has a list of all the undergraduate concentrations
+    link <- html_session("https://bulletin.brown.edu/the-college/concentrations/")
+    # Select the concentration of interest
+    link_conc <- link %>% follow_link(i+36)
+    # Read the content of the link
+    content <- read_html(link_conc)
+    # Scrape the table
+    link_table <- html_nodes(content, 'table')
+    # If the department doesn't display a table, an error "subscript out of bounds" appears. tryCatch will 
+    # ignore this error and allow the function to keep working
+    scrape_table <- tryCatch(html_table(link_table)[[1]], error=function(e) print(NA))
+    
+    
+    # Create a table only if the table exists (i.e. if scrape table ≠ NA)
+    if (is.na(scrape_table) == FALSE) {
+      # Convert the table into a dataframe  
+      classes <- scrape_table$X1
+      description <- scrape_table$X2
+      number_classes <- scrape_table$X3
+      test2 <- data_frame(classes, description, number_classes)
+    } else {stop('This department does not have a table of requirements')}
+    
+    
+  } else {stop('Please enter a valid concentration name. Refer to the list of undergraduate concentrations offered at Brown at https://bulletin.brown.edu/the-college/concentrations/')}
+  
+  
+## Concentration 2 -------------------
+table_2 <- function(conc_name2) {
+  ## Run the function only if the user's input matches the name listed in the concentration 
+  ## list, ignoring cases. 
+  match <- grepl(pattern=paste("^", conc_name2,"$", sep=""), conc_list, ignore.case=TRUE)
+  # Index to find the link of the concentraton of interest
+  i <- grep(pattern=paste("^", conc_name2,"$", sep=""), conc_list, ignore.case=TRUE)
+  if (any(match==TRUE) && str_length(conc_name2) == str_length(conc_list[i])) {
+    # Pull up the website that has a list of all the undergraduate concentrations
+    link <- html_session("https://bulletin.brown.edu/the-college/concentrations/")
+    # Select the concentration of interest
+    link_conc <- link %>% follow_link(i+36)
+    # Read the content of the link
+    content <- read_html(link_conc)
+    # Scrape the table
+    link_table <- html_nodes(content, 'table')
+    # If the department doesn't display a table, an error "subscript out of bounds" appears. tryCatch will 
+    # ignore this error and allow the function to keep working
+    scrape_table <- tryCatch(html_table(link_table)[[1]], error=function(e) print(NA))
+    
+    
+    # Create a table only if the table exists (i.e. if scrape table ≠ NA)
+    if (is.na(scrape_table) == FALSE) {
+      # Convert the table into a dataframe  
+      classes <- scrape_table$X1
+      description <- scrape_table$X2
+      number_classes <- scrape_table$X3
+      test2 <- data_frame(classes, description, number_classes)
+    } else {stop('This department does not have a table of requirements')}
+    
+    
+  } else {stop('Please enter a valid concentration name. Refer to the list of undergraduate concentrations offered at Brown at https://bulletin.brown.edu/the-college/concentrations/')}
+}  
+
+  
+conc_comp <- function{}
+table_req <- conc_req("Economics") 
+table_req <- conc_req("Economics") 
