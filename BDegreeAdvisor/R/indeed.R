63c336842cd628dcb87879e23bbd4a5f9670ff0a
Joyce Elias <joyce_eias@brown.edu>
2017-12-13 14:06:46 -0800

    updated the description final and finalized indeed.com scraping

--- BDegreeAdvisor/R/indeed.R
+++ BDegreeAdvisor/R/indeed.R
@@ -9,26 +9,26 @@
 #'**query is a character string and so it needs to be inputted with quotation marks ""
 #'@param loc: The location of jobs the user would like to search for. The input is not case sensitive
 #'**loc is a character string as well and so the input needs to be encased around quotation marks. ""
-#'@return Dataframe that contains the hiring company, the job title, the description, the location, and the job link 
+#'@return Table of a dataframe that contains the hiring company, the job title, the description, the location, and the job link 
 #'for the search results of that specific query
 #' @examples 
 #' job_finder("data analyst","Providence")
 #' job_finder("merchandise planner","07002")
+#' job_finder("graphic designer","02912")
 
 
 job_finder<-function(query,loc){
-  #Load the necessary libraries for this function to run
-  library(dplyr)
-  library(rvest)
 
-# Check to make sure the user entered the arguements in correctly
+  
+
+# Check to make sure the user entered the query arguement in correctly
   suppressWarnings(
     if(missing(query)){
       stop('Please enter a job title or keyword as a string.', call. = FALSE)
     }
   )
   
-  # check that the location variable has been inputted
+# check that the location variable has been inputted
   suppressWarnings(
     if(missing(loc)){
       stop('Please enter a city,state,or zip code as a string.', call. = FALSE)
@@ -36,7 +36,7 @@ job_finder<-function(query,loc){
   )
   
  
-  # This creates an empty dataframe to store our updating results in
+# This creates an empty dataframe to store our updating results in
   indeed_job_compiled<-data.frame("Hiring Company"=character(),
                                   "Job Title"=character(),
                                   "Description"=character(),
@@ -44,7 +44,7 @@ job_finder<-function(query,loc){
                                   "Job Link"=character())
                                   
   
-  # Splitting the url into parts to view 10 pages of search results. I capped the number of pages for size purposes.
+# Splitting the url into parts to view 10 pages of search results. I capped the number of pages for size purposes.
   b= seq(from= 10, to= 100, by=10)
   urls=vector(length=length(b)+1)
   
@@ -53,37 +53,41 @@ job_finder<-function(query,loc){
     url_part2<-"&l="
     url_part3<-"&start="  
     
+    # paste together a url to webscrape. The url will depend on the user's inputs
     url<-paste0(url_part1,query,url_part2,loc,url_part3,b[i])  
-    
+    # Wherever there is a space, replace it with a "+" sign. This is how indeed.com lays out their search pages
     urls[i+1]<-gsub(pattern=" ",replacement = "+",url) 
   }
+  # The first page of any query does not have the addition of url_part3, and so we specify this unique url here
   first_page<-paste0(url_part1,query,url_part2,loc)
   urls[1]<-gsub(pattern=" ",replacement = "+",first_page) 
  
 # Launch an Html session and web scrape information about each job posting 
-  for ( j in 1:length(urls)){
+ 
+   for ( j in 1:length(urls)){
   session1<-html_session(urls[j])
     
-  # grab the titles of the jobs
+# grab the titles of the jobs
   job_titles<-session1 %>%
       html_nodes("[data-tn-element=jobTitle]") %>%
       html_text() 
     
-  # The name of the Organization that is hiring
+# The name of the Organization that is hiring
     company_names<-session1 %>%
       html_nodes(".company") %>%
       html_text()
     
-  # Where is the job located ?
+# Where is the job located ?
     location<-session1 %>%
       html_nodes(".location") %>%
       html_text()
-  # Job Description
+
+# Job Description
     description <- session1 %>%
       html_nodes(".summary") %>%
       html_text()
     
-    # Give the user a link to the job so that they can apply to it later
+# Give the user a link to the job so that they can apply to it later
     job_link<- session1%>%
       html_nodes(css= "[data-tn-element=jobTitle]") %>%
       html_attr("href")
@@ -101,12 +105,12 @@ job_finder<-function(query,loc){
     indeed_job_compiled<- rbind(indeed_job_compiled,data.frame(company_names,job_titles,description,location,job_link))
   }
     
-  library(pander)
+ 
   pandoc.table(indeed_job_compiled)
 }
   
 
-job_finder("data analyst","providence,RI")
+
 
 
  
