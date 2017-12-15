30e0df88c070d86fd6b28b6629a0e6b170bf40e7
sullivanstatistics <adam@sullivanstatistics.com>
2017-12-14 23:09:11 -0500

    Fixed Namespace and function errors

--- BDegreeAdvisor/R/conc_req.R
+++ BDegreeAdvisor/R/conc_req.R
@@ -1,10 +1,10 @@
 #' conc_req Function
 #'
-#' This function takes the concentration of interest as an input and returns a 
+#' This function takes the concentration of interest as an input and returns a
 #' table with all the required classes for that particular concentration.
-#' @param conc.name:concentration name as it appears in the list of Brown concentrations in the 
-#' website: "https://bulletin.brown.edu/the-college/concentrations/". The input is not case 
-#' sensitive. **conc.name is a character string, so it needs to go with "". 
+#' @param conc.name:concentration name as it appears in the list of Brown concentrations in the
+#' website: "https://bulletin.brown.edu/the-college/concentrations/". The input is not case
+#' sensitive. **conc.name is a character string, so it needs to go with "".
 #' @return Table of all the required courses for the concentration inputed.
 #' @examples
 #' conc_req("Economics")
@@ -14,22 +14,22 @@
 
 
 conc_req <- function(conc_name) {
-  # This function takes the concentration of interest as an input and returns a 
-  # table with all the required classes for that particular concentration. The input is not 
-  # case sensitive. The function will run only if the input matches the concentration name listed 
-  # on the website. If the department does not display a table, a message will display this. 
-  
-  # Compile a list of undergraduate concentrations available at Brown from the website, so 
+  # This function takes the concentration of interest as an input and returns a
+  # table with all the required classes for that particular concentration. The input is not
+  # case sensitive. The function will run only if the input matches the concentration name listed
+  # on the website. If the department does not display a table, a message will display this.
+
+  # Compile a list of undergraduate concentrations available at Brown from the website, so
   # that if the concentrations are updated on the website, the list is also updated
-  link <- html_session("https://bulletin.brown.edu/the-college/concentrations/") 
-  conc_list <- link %>% 
+  link <- html_session("https://bulletin.brown.edu/the-college/concentrations/")
+  conc_list <- link %>%
     html_nodes("#textcontainer li") %>% # css selector for the entire list of concentration
-    html_text() # select only the text 
+    html_text() # select only the text
   conc_list <- as.vector(conc_list)
-  
-  
-  ## Run the function only if the user's input matches the name listed in the concentration 
-  ## list, ignoring cases. 
+
+
+  ## Run the function only if the user's input matches the name listed in the concentration
+  ## list, ignoring cases.
   match <- grepl(pattern=paste("^", conc_name,"$", sep=""), conc_list, ignore.case=TRUE)
   # Index to find the link of the concentraton of interest
   i <- grep(pattern=paste("^", conc_name,"$", sep=""), conc_list, ignore.case=TRUE)
@@ -42,20 +42,20 @@ conc_req <- function(conc_name) {
     content <- read_html(link_conc)
     # Scrape the table
     link_table <- html_nodes(content, 'table')
-    # If the department doesn't display a table, an error "subscript out of bounds" appears. tryCatch will 
+    # If the department doesn't display a table, an error "subscript out of bounds" appears. tryCatch will
     # ignore this error and allow the function to keep working
     scrape_table <- tryCatch(html_table(link_table)[[1]], error=function(e) matrix(nrow=2, ncol=2))
-    
-    # Create a table only if the table exists (i.e. if scrape table ≠ NA)
+
+    # Create a table only if the table exists (i.e. if scrape table != NA)
     if (is.na(scrape_table[1,1]) == FALSE) {
-      # Convert the table into a dataframe  
+      # Convert the table into a dataframe
       Class <- scrape_table$X1
       Names <- scrape_table$X2
       Credit <- scrape_table$X3
       tab <- data_frame(Class, Names, Credit)
       return(tab)
     } else {stop('This department does not have a table of requirements')}
-    
-    
+
+
   } else {stop('Please enter a valid concentration name. Refer to the list of undergraduate concentrations offered at Brown at https://bulletin.brown.edu/the-college/concentrations/')}
-}  
+}
