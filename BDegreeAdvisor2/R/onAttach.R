9bd84af7635501a44efa16140bf660c289361286
matsuim <mcm710@gmail.com>
2017-12-14 23:12:15 -0500

    remove second package

--- BDegreeAdvisor2/R/onAttach.R
+++
@@ -1,2 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Welcome to BDegree Advisor. We hope this package facilitates the design of your education plan at Brown by allowing you to organize your concentration requirements, explore Brown's Open Curriculum, and expand your career options for the summer or after graduation.")}
